b427f46ee5d9200c48eae36fff80f071776a1dd9
Mathijs Vogelzang <mathijs@gmail.com>
2011-10-28 16:47:13 +0200

    Launch javadoc

---
+++ javadoc/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "com.appbrain", "com/appbrain/package-summary.html", [ [ "Interfaces", null, [ [ "AdService", "com/appbrain/AdService.html", null, "" ], [ "RemoteSettings", "com/appbrain/RemoteSettings.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AppBrain", "com/appbrain/AppBrain.html", null, "" ], [ "ReferrerReceiver", "com/appbrain/ReferrerReceiver.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ javadoc/reference/lists.js
@@ -0,0 +1,8 @@
+var DATA = [
+      { id:0, label:"com.appbrain", link:"com/appbrain/package-summary.html", type:"package" },
+      { id:1, label:"com.appbrain.AdService", link:"com/appbrain/AdService.html", type:"class" },
+      { id:2, label:"com.appbrain.AppBrain", link:"com/appbrain/AppBrain.html", type:"class" },
+      { id:3, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
+      { id:4, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
+
+    ];
