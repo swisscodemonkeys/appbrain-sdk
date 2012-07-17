1c0509fda48ed0431dc3e228e23cbdd95fea0171
Mathijs Vogelzang <mathijs@gmail.com>
2012-07-17 17:48:51 +0200

    New SDK release: - AppLift banner ads - Translation for many more languages of interstitial and offerwall - Deprecate maybeShowOfferwall and showDialog - Various bugfixes

--- javadoc/reference/lists.js
+++ javadoc/reference/lists.js
@@ -2,7 +2,9 @@ var DATA = [
       { id:0, label:"com.appbrain", link:"com/appbrain/package-summary.html", type:"package" },
       { id:1, label:"com.appbrain.AdService", link:"com/appbrain/AdService.html", type:"class" },
       { id:2, label:"com.appbrain.AppBrain", link:"com/appbrain/AppBrain.html", type:"class" },
-      { id:3, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
-      { id:4, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
+      { id:3, label:"com.appbrain.AppBrainBanner", link:"com/appbrain/AppBrainBanner.html", type:"class" },
+      { id:4, label:"com.appbrain.AppBrainBanner.Listener", link:"com/appbrain/AppBrainBanner.Listener.html", type:"class" },
+      { id:5, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
+      { id:6, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
 
     ];
