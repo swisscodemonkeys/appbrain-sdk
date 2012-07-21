edfbe01d0c534b3015d8501064221785f85ae32c
Mathijs Vogelzang <mathijs@gmail.com>
2012-07-21 16:37:09 +0200

    Fix banner API

--- javadoc/assets/navtree_data.js
+++ javadoc/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "com.appbrain", "com/appbrain/package-summary.html", [ [ "Interfaces", null, [ [ "AdService", "com/appbrain/AdService.html", null, "" ], [ "AppBrainBanner.Listener", "com/appbrain/AppBrainBanner.Listener.html", null, "" ], [ "RemoteSettings", "com/appbrain/RemoteSettings.html", null, "" ] ]
+[ [ "com.appbrain", "com/appbrain/package-summary.html", [ [ "Interfaces", null, [ [ "AdService", "com/appbrain/AdService.html", null, "" ], [ "BannerListener", "com/appbrain/BannerListener.html", null, "" ], [ "RemoteSettings", "com/appbrain/RemoteSettings.html", null, "" ] ]
 , "" ], [ "Classes", null, [ [ "AppBrain", "com/appbrain/AppBrain.html", null, "" ], [ "AppBrainBanner", "com/appbrain/AppBrainBanner.html", null, "" ], [ "ReferrerReceiver", "com/appbrain/ReferrerReceiver.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
--- javadoc/reference/lists.js
+++ javadoc/reference/lists.js
@@ -3,7 +3,7 @@ var DATA = [
       { id:1, label:"com.appbrain.AdService", link:"com/appbrain/AdService.html", type:"class" },
       { id:2, label:"com.appbrain.AppBrain", link:"com/appbrain/AppBrain.html", type:"class" },
       { id:3, label:"com.appbrain.AppBrainBanner", link:"com/appbrain/AppBrainBanner.html", type:"class" },
-      { id:4, label:"com.appbrain.AppBrainBanner.Listener", link:"com/appbrain/AppBrainBanner.Listener.html", type:"class" },
+      { id:4, label:"com.appbrain.BannerListener", link:"com/appbrain/BannerListener.html", type:"class" },
       { id:5, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
       { id:6, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
 
