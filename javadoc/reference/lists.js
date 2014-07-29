0b36b7f7361d70e72362fa0f5fe421582fda2a61
Mathijs Vogelzang <mathijs@gmail.com>
2014-07-29 15:58:05 +0200

    Update javadoc

--- javadoc/reference/lists.js
+++ javadoc/reference/lists.js
@@ -1,11 +1,12 @@
 var DATA = [
       { id:0, label:"com.appbrain", link:"com/appbrain/package-summary.html", type:"package" },
-      { id:1, label:"com.appbrain.AdService", link:"com/appbrain/AdService.html", type:"class" },
-      { id:2, label:"com.appbrain.AdvertiserService", link:"com/appbrain/AdvertiserService.html", type:"class" },
-      { id:3, label:"com.appbrain.AppBrain", link:"com/appbrain/AppBrain.html", type:"class" },
-      { id:4, label:"com.appbrain.AppBrainBanner", link:"com/appbrain/AppBrainBanner.html", type:"class" },
-      { id:5, label:"com.appbrain.BannerListener", link:"com/appbrain/BannerListener.html", type:"class" },
-      { id:6, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
-      { id:7, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
+      { id:1, label:"com.appbrain.AdOptions", link:"com/appbrain/AdOptions.html", type:"class" },
+      { id:2, label:"com.appbrain.AdService", link:"com/appbrain/AdService.html", type:"class" },
+      { id:3, label:"com.appbrain.AdvertiserService", link:"com/appbrain/AdvertiserService.html", type:"class" },
+      { id:4, label:"com.appbrain.AppBrain", link:"com/appbrain/AppBrain.html", type:"class" },
+      { id:5, label:"com.appbrain.AppBrainBanner", link:"com/appbrain/AppBrainBanner.html", type:"class" },
+      { id:6, label:"com.appbrain.BannerListener", link:"com/appbrain/BannerListener.html", type:"class" },
+      { id:7, label:"com.appbrain.ReferrerReceiver", link:"com/appbrain/ReferrerReceiver.html", type:"class" },
+      { id:8, label:"com.appbrain.RemoteSettings", link:"com/appbrain/RemoteSettings.html", type:"class" }
 
     ];
